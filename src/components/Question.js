043a7993472513a5b463ffbe61bb91c52d14888f
Maddie Weber <maddieweber7@gmail.com>
2023-11-27 15:42:31 -0500

    had to look at solution but will practice again

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useState,  useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=> {
+    if (timeRemaining===0) {
+      setTimeRemaining(10)
+      onAnswered(false); //making sure to not count as a point
+      return; // exit early
+    }
+    //setup timeout to run after one second 
+  const timerId = setTimeout(()=> {
+    //decrement the time remaining
+    setTimeRemaining((timeRemaining)=> timeRemaining-1);
+  }, 1000);
+
+  //cleanup after the timeout in case the component unmounts before the timer is done
+  return function() {
+    clearTimeout(timerId);
+  };
+}, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
-
   const { id, prompt, answers, correctIndex } = question;
 
   return (
@@ -27,6 +43,6 @@ function Question({ question, onAnswered }) {
       <h5>{timeRemaining} seconds remaining</h5>
     </>
   );
-}
+  }
 
 export default Question;
